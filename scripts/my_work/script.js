65eb5be150c76c3155f7707181a3364cd9432d54
holyman2k <charlie.y.wu@gmail.com>
2012-02-28 04:18:54 +0000

    "modified lazy image loading"

--- scripts/my_work/script.js
+++ scripts/my_work/script.js
@@ -1,3 +1,11 @@
 $(function() {
-    $("img").lazyload();
+    $("img.lazy").width(560);
+    $("img.lazy").lazyload({
+        effect : "fadeIn",
+        event : "click"
+    });
+});
+
+$(document).ready(function(){
+    $("img.lazy").attr("src", "images/grey.gif");
 });
